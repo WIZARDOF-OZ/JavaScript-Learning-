e3c55672165dca75a95c3467435235ebb3401b70
WIZARD OF OZ <nurajabegum81@gmail.com>
2021-12-31 21:13:26 +0530

    Manipulating Websites Using JS Window Object

---
+++ js/tut11.js
@@ -0,0 +1,21 @@
+//Manipulating Websites Using JS Window Object
+console.log("Welcome to tut11");
+
+let win= window.document;
+
+// win.alert("hello")
+// win = prompt('Please Type Your Name')
+
+// win = confirm("Are you want to destroy you web?");
+//win = window.innerHeight;
+// win = window.innerWidth; 
+// win = location.toString();
+// win = window.history;
+// win = history.length
+// history.go(1)
+// history.go(-1)
+
+console.log(win);
+
+
+
