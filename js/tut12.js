9d67ee0496fa514f58750ca9f1e7e81dbadf5531
WIZARD OF OZ <nurajabegum81@gmail.com>
2022-01-05 19:19:36 +0530

    Understanding DOM & Creating a Website Layout

---
+++ js/tut12.js
@@ -0,0 +1,21 @@
+console.log("Welcome to tut12");
+
+let win = document;
+// win = document.all;
+// win = document.body;
+// win = document.domain;
+// win = document.forms;
+// win = document.forms[0];
+// Array.from(document).forEach(function (element) {
+//     console.log(element);
+// })
+// win = document.links[0].href;
+Array.from(document.images).forEach(function (element,index,array) {
+    console.log(element,index,array)
+})
+
+Array.from(document.scripts).forEach(function (element,index,array) {
+    console.log(element,index,array)
+})
+
+ console.log(win)
